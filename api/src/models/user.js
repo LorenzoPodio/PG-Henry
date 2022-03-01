9ee34ba84e4571f5d7eaf78b656d545293c9a029
gregoconst <gregorio.constante12@gmail.com>
2022-03-01 16:20:36 -0300

    model user

---
+++ api/src/models/user.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('user', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        user: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }
+    );
+};
