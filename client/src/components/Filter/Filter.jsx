3fe15c928e8799d2d70cbe8bc7992227ae20d5f2
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-02 14:20:20 -0300

    Componente Filter

---
+++ client/src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+// Componente que retorna un select con todas las opciones para filtrar
+
+export default function Filter({ items, defaultDescription, handleFilter, filterType }) {
+  // handleFilter: Función que llama a la ruta del back con el filtro correspondiente, recibe como parametro el valor seleccionado
+  // items: Opciones para filtrar
+  //fitlerType: String --> date, excursionType o location
+
+  return (
+    <div>
+      <select
+        name={{ defaultDescription }}
+        onChange={(e) => handleFilter(e.target.value)}
+      >
+        <option value="default">{defaultDescription}</option>
+        {items?.map((item) => {
+          return (
+            <option key={item.location} value={item[filterType]}>
+              {item[filterType]}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
