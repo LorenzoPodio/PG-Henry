8beae75c3353674dcad00984eb3fd1dd4eb5be54
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-03 15:55:45 -0300

    Se modifico el componente Excursiones para implementar los filtros

--- client/src/components/Excursions/Excursions.jsx
+++ client/src/components/Excursions/Excursions.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { useExcursionsContext } from '../../context/ExcursionsContext';
+import React from "react";
+import { useExcursionsContext } from "../../context/ExcursionsContext";
+import Filter from "../Filter/Filter";
 
 export const Excursions = () => {
-  const {getExcursions, data} = useExcursionsContext();
-
-  getExcursions();
+  const {
+    data,
+    handlerFilterByLocation,
+    handlerFilterByDate,
+    handlerFilterByType,
+    allExcursions,
+  } = useExcursionsContext();
 
   return (
     <div>
       <h1>Excursiones</h1>
+      <Filter
+        items={allExcursions}
+        defaultDescription="Locación"
+        handleFilter={handlerFilterByLocation}
+        filterType="location"
+      />
 
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <Filter
+        items={allExcursions}
+        defaultDescription="Fechas"
+        handleFilter={handlerFilterByDate}
+        filterType="date"
+      />
+
+      <Filter
+        items={allExcursions}
+        defaultDescription="Tipo de excursión"
+        handleFilter={handlerFilterByType}
+        filterType="excursionType"
+      />
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
