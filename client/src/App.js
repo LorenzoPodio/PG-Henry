fe25dc21ed916e0ededad3e91d31102cb42d6f88
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-03-03 18:48:14 -0300

    Creación del componente ExcursionCard y ExcursionDetail

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,18 @@ import { NavBar } from './components/NavBar/NavBar.jsx';
 import Login from './components/Login/Login';
 import Register from './components/Registro/Register'
 import { Prices } from './components/Prices/Prices';
+import { ExcursionDetail } from "./components/ExcursionDetail/ExcursionDetail.jsx";
 
 function App() {
   return (
     <div>
-        {<NavBar/>}
+      <NavBar/>
       <Routes>
-        <Route exact path='/' element={<Excursions/>}/>
+        <Route exact path='/Excursiones' element={<Excursions/>}/>
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/registro' element={<Register/>}/>
-        <Route path='/Tarifas' element={<Prices/>}/>
-        <Route/>
-
+        <Route path='/tarifas' element={<Prices/>}/>
+        <Route path='/excursion/detalle/:id' element={<ExcursionDetail/>}/>
       </Routes>
     </div>
   );
--- client/src/components/Excursions/Excursions.jsx
+++ client/src/components/Excursions/Excursions.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useExcursionsContext } from "../../context/ExcursionsContext";
 import Filter from "../Filter/Filter";
+import { ExcursionCard } from "../ExcursionCard/ExcursionCard";
 
 export const Excursions = () => {
   const {
@@ -11,31 +12,53 @@ export const Excursions = () => {
     allExcursions,
   } = useExcursionsContext();
 
+  console.log('data :>> ', data);
+  console.log('allExcursions', allExcursions);
+
   return (
     <div>
       <h1>Excursiones</h1>
-      <Filter
-        items={allExcursions}
-        defaultDescription="Locación"
-        handleFilter={handlerFilterByLocation}
-        filterType="location"
-      />
-
-      <Filter
-        items={allExcursions}
-        defaultDescription="Fechas"
-        handleFilter={handlerFilterByDate}
-        filterType="date"
-      />
-
-      <Filter
-        items={allExcursions}
-        defaultDescription="Tipo de excursión"
-        handleFilter={handlerFilterByType}
-        filterType="excursionType"
-      />
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className="inline-flex">
+        <Filter
+          items={allExcursions}
+          defaultDescription="Locación"
+          handleFilter={handlerFilterByLocation}
+          filterType="location"
+        />
+        <Filter
+          items={allExcursions}
+          defaultDescription="Fechas"
+          handleFilter={handlerFilterByDate}
+          filterType="date"
+        />
+        <Filter
+          items={allExcursions}
+          defaultDescription="Tipo de excursión"
+          handleFilter={handlerFilterByType}
+          filterType="excursionType"
+        />
+      </div>
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {
+              data && data.map(e => (
+                <ExcursionCard
+                  key={e.id}
+                  id={e.id}
+                  name={e.name}
+                  primaryImage={e.Images[0]}
+                  location={e.location}
+                  date={e.date.join(', ')}
+                  price={e.price}
+                  excursionType={e.excursionType}
+                />
+              ))
+            }
+          </div>
+        </div>
+      </div >
     </div>
   );
 };
