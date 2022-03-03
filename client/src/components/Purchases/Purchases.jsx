b88552d0ecc6bbc59ab29ec293338c90d1c745b1
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-03 20:22:51 -0300

    Estructura basica del componente Purchases

---
+++ client/src/components/Purchases/Purchases.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Purchases = () => {
+  return (
+    <div>
+      <h1>Sección en construcción...</h1>
+      <div>
+        <img src="https://i.imgur.com/duM495V.png" alt="trabajando" />
+      </div>
+    </div>
+  );
+};
+
+export default Purchases
