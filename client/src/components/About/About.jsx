8d1ef6f735c746052bee033830cff96701ef093a
gregoconst <gregorio.constante12@hotmail.com>
2022-03-03 16:12:01 -0300

    componente about (sin logica)

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>Sección en construcción...</h1>
+      <div>
+        <img src="shorturl.at/foqP0" alt="trabajando" />
+      </div>
+    </div>
+  );
+};
