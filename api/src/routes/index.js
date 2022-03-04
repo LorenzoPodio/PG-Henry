c905b5f5180a1473b5996bb036b44e3c4a3f5bd7
lautarocoello <lautarocoello@gmail.com>
2022-03-04 18:38:59 -0300

    ruta cambios datos admin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,10 +5,10 @@ const addAdmin = require('./addAdmin')
 const getExcursion = require('./getExcursion')
 const deleteExcursion = require('./deleteExcursion');
 const putExcursion = require('./putExcursion');
+const changeDatesAdmin = require('./putDataAdmin');
 
 
-
-
+router.use("/changeadmin", changeDatesAdmin)
 router.use("/changeexcursion", putExcursion)
 router.use("/deleteexcursion", deleteExcursion)
 router.use("/getexcursion", getExcursion)
