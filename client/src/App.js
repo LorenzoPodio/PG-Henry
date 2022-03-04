70d731754a8ecb67848dfd0ba529c96865116887
nabratte <61842410+nabratte@users.noreply.github.com>
2022-03-04 17:54:45 -0300

    Componente de creacion

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Excursions } from './components/Excursions/Excursions';
+import { ExcursionsPost } from './components/ExcursionsPost/ExcursionsPost';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div>
       <Routes>
         <Route exact path='/' element={<Excursions/>}/>
-        <Route/>
+        <Route exact path='/crearExcursion' element={<ExcursionsPost/>}/>
         <Route/>
       </Routes>
     </div>
