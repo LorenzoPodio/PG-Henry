bca873c15066697179bf3e122a36d7c9730906b0
Damian Yerien <damian_yerien@hotmail.com>
2022-03-09 11:25:19 -0300

    nuevas rutas

---
+++ api/src/models/product.js
@@ -0,0 +1,2 @@
+//   ID excursion dia fecha horario stock price
+ //  1   rio manso  lu   14   10     "20"  del front    aparte mandar cantidad de plazas del front
--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,42 +1,42 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('user', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
+// const { DataTypes } = require('sequelize');
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//     // defino el modelo
+//     sequelize.define('user', {
+//         name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
 
-        },
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        adress: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        dni: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            unique: true
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-    }
-    );
-};
+//         },
+//         id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             allowNull: false,
+//             autoIncrement: true,
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         adress: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         dni: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             unique: true
+//         },
+//         lastName: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true
+//         },
+//     }
+//     );
+// };
