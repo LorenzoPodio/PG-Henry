1ebfc185964d4f55245b879e8817d7c015d66109
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-09 23:08:50 -0300

    Implementación de mercado pago en el back

---
+++ api/src/routes/mercadopago.js
@@ -0,0 +1,56 @@
+const { Router } = require("express");
+const mp = Router();
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+
+mercadopago.configure({
+  access_token:
+  "TEST-5919072404673194-030821-f3180fd391d21f8aa61f48abfaf2624d-1086472471",
+});
+
+mp.post("/", function (req, res) {
+  let preference = {
+    items: [
+      {
+        title: "ItemPrueba",
+        unit_price: 100,
+        quantity: 1,
+      },
+    ],
+    external_reference : "1",
+    payment_methods: {
+      excluded_payment_types: [
+        {
+          id: "atm"
+        }
+      ],
+      installments: 3  //Cantidad máximo de cuotas
+    },
+    back_urls: {
+      success: "http://localhost:3001/mercadopago/feedback",
+      failure: "http://localhost:3001/mercadopago/feedback",
+      pending: "http://localhost:3001/mercadopago/feedback",
+    },
+    auto_return: "approved",
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({ id: response.body.id });
+      // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+mp.get("/feedback", function (req, res) {
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id,
+  });
+});
+
+module.exports = mp;
