c995dfaa2043cc8663ff87c06d8925874d983826
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-09 23:22:02 -0300

    Mercado pago implementado en el front.

--- client/src/components/AdminPanel/AdminPanel.jsx
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -2,14 +2,34 @@ import { useExcursionsContext } from "../../context/ExcursionsContext";
 import { PencilIcon, TrashIcon, PlusCircleIcon } from "@heroicons/react/solid";
 import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Checkout from "../MercadoPago/Checkout";
 
 export const AdminPanel = () => {
+  const [data, setData] = useState(""); //Estado para setear la respuesta de mercado pago
   const { allExcursions, deleteExcursion } = useExcursionsContext();
   const navigate = useNavigate();
 
+  //  IMPLEMENTACION DE MP
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/mercadopago")
+      .then((data) => {
+        setData(data.data);
+        console.info("Contenido de data:", data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  const products = [
+    { title: "Producto 1", quantity: 5, price: 10.52 },
+    { title: "Producto 2", quantity: 15, price: 100.52 },
+    { title: "Producto 3", quantity: 6, price: 200 },
+  ];
+  ////////////////////
 
-  function handleEdit(e){
-    navigate(`/editarExcursion?id=${e.target.value}&name=${e.target.name}`)
+  function handleEdit(e) {
+    navigate(`/editarExcursion?id=${e.target.value}&name=${e.target.name}`);
   }
 
   function handleDelete(e) {
@@ -30,58 +50,65 @@ export const AdminPanel = () => {
   }
 
   return (
-
-          <div className="grid place-content-center" id="top">
-            <h1 className="grid place-content-center mt-0">PANEL DE CONTROL</h1>
-            <div className="flex flex-col w-fit m-10">
-
-              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table className="min-w-full divide-y divide-gray-200">
-                      <thead className="bg-gray-50">
-                        <tr>
-                          <th
-                            scope="col"
-                            className="px-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            nombre
-                          </th>
-                          <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            locacion
-                          </th>
-                          <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            tipo
-                          </th>
-                          <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            precio
-                          </th>
-                          <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            editar
-                          </th>
-                          <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            borrar
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="bg-white divide-y divide-gray-200">
-                      {allExcursions &&
-                      allExcursions.map((e) => {
+    <div className="grid place-content-center" id="top">
+      <h1 className="grid place-content-center mt-0">PANEL DE CONTROL</h1>
+      {/* Implementacion de mercado pago, descomentar cuando se utilice */}
+      {/* <div>
+        {!data ? (
+          <p>Aguarde un momento....</p>
+        ) : (
+          <Checkout products={products} data={data} />
+        )}
+      </div> */}
+      {/* ////////////////////////////////////////////////////////////// */}
+      <div className="flex flex-col w-fit m-10">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      nombre
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      locacion
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      tipo
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      precio
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      editar
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      borrar
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {allExcursions &&
+                    allExcursions.map((e) => {
                       return (
                         <tr key={e.name}>
                           <td className="px-6 py-4 whitespace-nowrap">
@@ -123,7 +150,6 @@ export const AdminPanel = () => {
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             <span className="hidden sm:block">
-
                               <button
                                 onClick={(e) => handleDelete(e)}
                                 type="button"
