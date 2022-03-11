f1e9eb19da2f1c3d01e73e8784db9a0d8ce73430
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-11 01:08:20 -0300

    Se crearon la estructura de los componentes Stepper, contenedor de OrderReview, Payment y PersonalDetails.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-
 import { About } from "./components/About/About";
 import Purchases from "./components/Purchases/Purchases";
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Excursions } from './components/Excursions/Excursions';
-import { ExcursionsPost } from './components/ExcursionsPost/ExcursionsPost';
-import Login from './components/Login/Login';
-import Register from './components/Registro/Register'
-import { Prices } from './components/Prices/Prices';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Excursions } from "./components/Excursions/Excursions";
+import { ExcursionsPost } from "./components/ExcursionsPost/ExcursionsPost";
+import Login from "./components/Login/Login";
+import Register from "./components/Registro/Register";
+import { Prices } from "./components/Prices/Prices";
 import { ExcursionDetail } from "./components/ExcursionDetail/ExcursionDetail.jsx";
-import { AdminPanel } from './components/AdminPanel/AdminPanel'
+import { AdminPanel } from "./components/AdminPanel/AdminPanel";
 import { Footer } from "./components/Footer/Footer";
-import { NavBar2 } from './components/NavBar/NavBar2'
+import { NavBar2 } from "./components/NavBar/NavBar2";
 import { Landing } from "./components/Landing/Landing";
 import { EditExcursion } from "./components/EditExcursion/EditExcursion";
+import Stepper from "./components/PurchaseSteps/Container/Stepper";
 
 function App() {
   return (
     <div>
-      <NavBar2/>
+      <NavBar2 />
       <Routes>
-        <Route exact path='/' element={<Landing/>}/>
-        <Route exact path='/crearExcursion' element={<ExcursionsPost/>}/>
-        <Route exact path='/editarExcursion' element={<EditExcursion/>}/>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/crearExcursion" element={<ExcursionsPost />} />
+        <Route exact path="/editarExcursion" element={<EditExcursion />} />
         <Route exact path="/excursiones" element={<Excursions />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/registro" element={<Register />} />
         <Route path="/tarifas" element={<Prices />} />
         <Route path="/compras" element={<Purchases />} />
         <Route path="/nosotros" element={<About />} />
-        <Route path='/excursion/detalle/:id' element={<ExcursionDetail/>}/>
-        <Route path='/panelAdmin' element={<AdminPanel/>}/>
+        <Route path="/excursion/detalle/:id" element={<ExcursionDetail />} />
+        <Route path="/panelAdmin" element={<AdminPanel />} />
+        <Route exact path="/checkout" element={<Stepper />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
