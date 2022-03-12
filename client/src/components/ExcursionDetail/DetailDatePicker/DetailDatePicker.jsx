7ea9b02523a5162f772114e22158991aba7c3be2
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-12 01:19:33 -0300

    Se implementó la lógica para llamar ruta del back selectProduct + implementación de addItemToCart

--- client/src/components/ExcursionDetail/DetailDatePicker/DetailDatePicker.jsx
+++ client/src/components/ExcursionDetail/DetailDatePicker/DetailDatePicker.jsx
@@ -1,69 +1,99 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import es from 'date-fns/locale/es';
-registerLocale('es', es)
+import es from "date-fns/locale/es";
+registerLocale("es", es);
 
 const daysCases = (array) => {
-  const arr = array.map(day => {
+  const arr = array.map((day) => {
     switch (day.toLowerCase()) {
-      case 'lunes':
-        return day = 1;
-      case 'martes':
-        return day = 2;
-      case 'miercoles':
-        return day = 3;
-      case 'jueves':
-        return day = 4;
-      case 'viernes':
-        return day = 5;
-      case 'sabado':
-        return day = 6;
+      case "lunes":
+        return (day = 1);
+      case "martes":
+        return (day = 2);
+      case "miercoles":
+        return (day = 3);
+      case "jueves":
+        return (day = 4);
+      case "viernes":
+        return (day = 5);
+      case "sabado":
+        return (day = 6);
       default:
-        return day = 0;
+        return (day = 0);
     }
   });
 
   return arr;
-}
+};
 
-export const DetailDatePicker = ({ excursionDays }) => {
-
-  const days = excursionDays? daysCases(excursionDays) : null;
+export const DetailDatePicker = ({ excursionDays, handleDate }) => {
+  const days = excursionDays ? daysCases(excursionDays) : null;
   const [startDate, setStartDate] = useState();
+
+  useEffect(() => {
+    handleDate(startDate); //  //Definida en ExcursionDetail para construir el item que sera agregado al cart
+  }, [startDate]);
 
   const isWeekday = (date) => {
     const day = date.getDay();
     switch (days.length) {
       case 2:
-        return day === days[0] || day === days[1]
+        return day === days[0] || day === days[1];
       case 3:
-        return day === days[0] || day === days[1] || day === days[2]
-        case 4:
-        return day === days[0] || day === days[1] || day === days[2] || day === days[3]
+        return day === days[0] || day === days[1] || day === days[2];
+      case 4:
+        return (
+          day === days[0] ||
+          day === days[1] ||
+          day === days[2] ||
+          day === days[3]
+        );
       case 5:
-        return day === days[0] || day === days[1] || day === days[2] || day === days[3] || day === days[4]
+        return (
+          day === days[0] ||
+          day === days[1] ||
+          day === days[2] ||
+          day === days[3] ||
+          day === days[4]
+        );
       case 6:
-        return day === days[0] || day === days[1] || day === days[2] || day === days[3] || day === days[4] || day === days[5]
+        return (
+          day === days[0] ||
+          day === days[1] ||
+          day === days[2] ||
+          day === days[3] ||
+          day === days[4] ||
+          day === days[5]
+        );
       case 7:
-        return day === days[0] || day === days[1] || day === days[2] || day === days[3] || day === days[4] || day === days[5] || day === days[6]
+        return (
+          day === days[0] ||
+          day === days[1] ||
+          day === days[2] ||
+          day === days[3] ||
+          day === days[4] ||
+          day === days[5] ||
+          day === days[6]
+        );
       default:
-        return day === days[0]
-    };
+        return day === days[0];
+    }
   };
-  
+
   return (
     <div>
-      <DatePicker className='shadow-md text-center rounded-md h-9 w-48'
-        dateFormat={'dd/MM/yyyy'}
-        placeholderText={'seleccione una fecha'}
+      <DatePicker
+        className="shadow-md text-center rounded-md h-9 w-48"
+        dateFormat={"dd/MM/yyyy"}
+        placeholderText={"seleccione una fecha"}
         selected={startDate}
         onChange={(date) => setStartDate(date)}
-        locale='es'
+        locale="es"
         filterDate={isWeekday}
         minDate={new Date()}
-        maxDate={new Date().setMonth(new Date().getMonth()+2)}
+        maxDate={new Date().setMonth(new Date().getMonth() + 2)}
       />
     </div>
-  )
-}
+  );
+};
--- client/src/components/ExcursionDetail/ExcursionDetail.jsx
+++ client/src/components/ExcursionDetail/ExcursionDetail.jsx
@@ -1,41 +1,100 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import { useExcursionsContext } from "../../context/ExcursionsContext";
-import {useCartContext} from "../../context/CartContext";
+import { useCartContext } from "../../context/CartContext";
 import { useParams } from "react-router-dom";
-import InputSelect from '../InputSelect/InputSelect';
-import { ShoppingCartIcon } from '@heroicons/react/solid'
-import { DetailDatePicker } from './DetailDatePicker/DetailDatePicker';
+import InputSelect from "../InputSelect/InputSelect";
+import { ShoppingCartIcon } from "@heroicons/react/solid";
+import { DetailDatePicker } from "./DetailDatePicker/DetailDatePicker";
+import axios from "axios";
 
 export const ExcursionDetail = () => {
+  const [item, setItem] = useState({}); //Estado para construir item y agregarlo al carrito
+  const [stock, setStock] = useState("0");
   const { id } = useParams();
-  const {
-    excursionByid,
-    getExcursionById
-  } = useExcursionsContext();
-
-  const {
-    addItemToCart
-  } = useCartContext();
-
+  const [disabled, setDisabled] = useState(true);
+  const { excursionByid, getExcursionById } = useExcursionsContext();
+  const { addItemToCart } = useCartContext();
 
   useEffect(() => {
-    getExcursionById(id)
-  }, [])
+    getExcursionById(id);
+    return () => {
+      //componentWillUnmount, para resetear el item cuando se fueran del detalle de la excursión.
+      setItem((prevState) => {
+        return {};
+      });
+    };
+  }, []);
+
+  //useEffect para llamar a la ruta del back selectProduct + setear el stock disponible.
+  useEffect(() => {
+    if (
+      item.hasOwnProperty("time") &&
+      item.hasOwnProperty("date") &&
+      item.hasOwnProperty("quantity")
+    ) {
+      return axios
+        .post("http://localhost:3001/selectProduct", {
+          ...item,
+          name: excursionByid.name,
+          price: excursionByid.price,
+          id: excursionByid.id,
+        })
+        .then((resp) => setStock(resp.data - item.quantity), setDisabled(false))
+        .catch((e) => {
+          return setStock(0), setDisabled(true);
+        });
+    }
+  }, [item]);
+
+  //Handles que construyen el item que se enviara a la ruta del back addCart
+  const handleChange = (e) => {
+    setItem((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+  const handleTime = (time) => {
+    setItem((prevState) => {
+      return { ...prevState, time };
+    });
+  };
+  const handleDate = (date) => {
+    setItem((prevState) => {
+      return { ...prevState, date };
+    });
+  };
 
   // const {Images, createdInDb, date, description, excursionType, extra, location, name, price, time} = excursionByid;
   return (
     <div className="md:flex items-start justify-center py-2 px-2">
       <div className="xl:w-2/6 lg:w-2/5 w-80 md:block hidden">
-        <img className="w-11/12" alt="img excursion" src={excursionByid?.Images[0]} />
-        <img className="mt-2 w-11/12" alt="img excursion" src={excursionByid?.Images[1]} />
+        <img
+          className="w-11/12"
+          alt="img excursion"
+          src={excursionByid?.Images[0]}
+        />
+        <img
+          className="mt-2 w-11/12"
+          alt="img excursion"
+          src={excursionByid?.Images[1]}
+        />
       </div>
       <div className="md:hidden">
-        <img className="w-full" alt="img excursion" src={excursionByid?.Images[0]} />
-        <img className="mt-6 w-full" alt="img excursion" src={excursionByid?.Images[1]} />
+        <img
+          className="w-full"
+          alt="img excursion"
+          src={excursionByid?.Images[0]}
+        />
+        <img
+          className="mt-6 w-full"
+          alt="img excursion"
+          src={excursionByid?.Images[1]}
+        />
       </div>
       <div className="md:w-3/5 lg:ml-8 md:ml-6 md:mt-0 mt-6">
         <div className="border-b border-gray-200 pb-3">
-          <p className="text-sm leading-none text-gray-600">{excursionByid?.location}</p>
+          <p className="text-sm leading-none text-gray-600">
+            {excursionByid?.location}
+          </p>
           <h1
             className="
 							lg:text-2xl
@@ -50,26 +109,52 @@ export const ExcursionDetail = () => {
             {excursionByid?.name}
           </h1>
         </div>
-        <div className='inline-flex w-full mb-2 border-b border-gray-200 items-center justify-start'>
+        <div className="inline-flex w-full mb-2 border-b border-gray-200 items-center justify-start">
           <div className="py-2 border-r border-l border-gray-200 flex items-center justify-around w-1/3">
             <p className="text-base leading-4 text-gray-800">Dia:</p>
             {/* {excursionByid?.date && <InputSelect options={excursionByid?.date}/>} */}
-            <DetailDatePicker excursionDays={excursionByid?.date}/>
+            <DetailDatePicker
+              handleDate={handleDate}
+              excursionDays={excursionByid?.date}
+            />
           </div>
           <div className="py-2 border-r border-gray-200 flex items-center justify-around w-1/3">
             <p className="text-base leading-4 text-gray-800">Hora:</p>
-            {excursionByid?.time && <InputSelect options={excursionByid?.time}/>}
+            {excursionByid?.time && (
+              <InputSelect
+                handleTime={handleTime}
+                options={excursionByid?.time}
+              />
+            )}
           </div>
           <div className="py-2 border-r border-gray-200 flex items-center justify-around w-1/3">
             <p className="text-base leading-4 text-gray-800">cantidad:</p>
-            <input type="number" name="cantidad" min={0} max={6} className='shadow-md text-center rounded-md h-9 w-1/3' />
+            <input
+              onChange={(e) => handleChange(e)}
+              type="number"
+              name="quantity"
+              min={0}
+              max={6}
+              className="shadow-md text-center rounded-md h-9 w-1/3"
+            />
           </div>
         </div>
         <div>
-          <p className="max-h-72 overflow-y-scroll text-sm lg:leading-tight leading-normal text-gray-600 mt-0">{excursionByid?.description}</p>
-          <p className="text-base leading-4 mt-4 text-gray-600">Tipo de Excursión: {excursionByid?.excursionType}</p>
-          <p className="text-base leading-4 mt-3 text-gray-600">Extra: {excursionByid?.extra}</p>
-          <p className="text-base font-bold leading-4 mt-3 mb-3 text-gray-600">$ {excursionByid?.price}</p>
+          <p className="max-h-72 overflow-y-scroll text-sm lg:leading-tight leading-normal text-gray-600 mt-0">
+            {excursionByid?.description}
+          </p>
+          <p className="text-base leading-4 mt-4 text-gray-600">
+            Tipo de Excursión: {excursionByid?.excursionType}
+          </p>
+          <p className="text-base leading-4 mt-3 text-gray-600">
+            Extra: {excursionByid?.extra}
+          </p>
+          <p className="text-base font-bold leading-4 mt-3 mb-3 text-gray-600">
+            $ {excursionByid?.price}
+          </p>
+          <p className="text-base font-bold leading-4 mt-3 mb-3 text-gray-600">
+            Stock: {stock}
+          </p>
         </div>
         <button
           className="
@@ -85,11 +170,21 @@ export const ExcursionDetail = () => {
 						py-4
 						hover:bg-gray-700
 					"
+          //Disabled, deshabilita el botón cuando el stock es 0.
+          disabled={disabled}
+          onClick={() =>
+            addItemToCart({
+              ...item,
+              name: excursionByid.name,
+              price: excursionByid.price,
+              id: excursionByid.id,
+            })
+          }
         >
           Agregar al Carrito
-          <ShoppingCartIcon className="w-5 h-5 ml-1"/>
+          <ShoppingCartIcon className="w-5 h-5 ml-1" />
         </button>
       </div>
     </div>
   );
-}
+};
