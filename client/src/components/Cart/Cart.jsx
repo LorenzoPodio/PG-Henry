8037dafccbc709808265e61cef54802100d912d1
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-03-15 23:37:44 -0300

    CARRITO: se mejoró estilos, mapeo de datos y se agregó funcion para borrar un producto del carrito

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -6,66 +6,64 @@ import { Link } from "react-router-dom";
 
 function Cart() {
   const { cartItems } = useCartContext();
+  console.log('CARTITEMS :>> ', cartItems);
+  let cartTotalPrice = 0;
+
+  cartItems?.forEach(e => {
+    cartTotalPrice += (e.price * e.quantity)
+  });
+
   return (
-    <div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>
-          {cartItems && typeof data !== "string" ? (
-            cartItems?.map((e, i) => {
+    <div className="flex flex-col">
+      <div>
+        {cartItems && typeof data !== "string" ? (
+          <div className="max-h-96 overflow-y-auto">
+            {cartItems?.map(e => {
               return (
                 <CartCard
-                  // id={e.id}
-                  key={i}
+                  key={e.detailId}
+                  detailId={e.detailId}
+                  orderId={e.orderId}
                   name={e.product.name}
-                  // images={e.Images}
-                  // location={e.location}
-                  date={e.date}
-                  price={e.price + `*` + e.quantity + ` = ` +e.price * e.quantity}
+                  images={e.product.Images}
+                  time={e.product.time}
+                  date={e.product.date}
+                  price={e.price}
                   quantity={e.quantity}
-                  // excursionType={e.excursionType}
                 />
               );
-            })
-          ) : (
-            <div>
-              <div className="w-full lg:w-1/2">
-                <img
-                  className="hidden lg:block"
-                  src="https://i.ibb.co/v30JLYr/Group-192-2.png"
-                  alt=""
-                />
-                <img
-                  className="hidden md:block lg:hidden"
-                  src="https://i.ibb.co/c1ggfn2/Group-193.png"
-                  alt=""
-                />
-                <img
-                  className="md:hidden"
-                  src="https://i.ibb.co/8gTVH2Y/Group-198.png"
-                  alt=""
-                />
-              </div>
-              <div>
-                <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">
-                  No hay contenido para mostrar 😬{" "}
-                </h1>
-                <p className="py-4 text-base text-gray-800">
-                  Ponete en contacto con nosotros si pensas que deberíamos
-                  agregar un nuevo viaje
-                </p>
-                <p className="py-2 text-base text-gray-800">
-                  Podes probar cambiando los filtros nuevamente!
-                </p>
-                <button className="w-full lg:w-auto my-4 border rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                  Escribinos!
-                </button>
-              </div>
+            })}
+          </div>
+        ) : (
+          <div>
+            <div className="w-full lg:w-1/2">
+              <img className="hidden lg:block" src="https://i.ibb.co/v30JLYr/Group-192-2.png" alt="" />
+              <img className="hidden md:block lg:hidden" src="https://i.ibb.co/c1ggfn2/Group-193.png" alt="" />
+              <img className="md:hidden" src="https://i.ibb.co/8gTVH2Y/Group-198.png" alt="" />
             </div>
-          )}
-        </div>
+            <div>
+              <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">
+                No hay contenido para mostrar 😬{" "}
+              </h1>
+              <p className="py-4 text-base text-gray-800">
+                Ponete en contacto con nosotros si pensas que deberíamos
+                agregar un nuevo viaje
+              </p>
+              <p className="py-2 text-base text-gray-800">
+                Podes probar cambiando los filtros nuevamente!
+              </p>
+              <button className="w-full lg:w-auto my-4 border rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                Escribinos!
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-      <Link to="/checkout">
-        <button className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-400 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+      <div className="mt-2 text-center text-lg font-bold rounded-md bg-slate-900 text-white">
+        <p>TOTAL DEL CARRITO: ${cartTotalPrice}</p>
+      </div>
+      <Link to="/checkout" className="text-center">
+        <button className="px-4 py-2 my-2 rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-600">
           Terminar compra
         </button>
       </Link>
