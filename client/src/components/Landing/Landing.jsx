b44605a0f1caa52019b05f273b89a762b1c34404
nabratte <61842410+nabratte@users.noreply.github.com>
2022-03-19 16:22:01 -0300

    Form de contacto y algunos estilos

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,17 +1,21 @@
-
-import React from 'react';
-import './Landing.css'
-import fondo from './pexels-lumn-167699.jpg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Landing.css";
+import fondo from "./pexels-lumn-167699.jpg";
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
-  
-    return (
-        <div className='containerLanding'>
-            <img className='fondoLanding' alt="nnn" src={fondo}/>
-            <Link to="/excursiones">
-            <button className="inline-block text-center bg-sky-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-sky-700 buttonlanding">Ver Excursiones</button>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="containerLanding h-full">
+      <img
+        className="fondoLanding h-screen w-full"
+        alt="image not found"
+        src={fondo}
+      />
+      <Link to="/excursiones">
+        <button className="inline-block text-center bg-sky-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-sky-700 buttonlanding">
+          Ver Excursiones
+        </button>
+      </Link>
+    </div>
+  );
 };
