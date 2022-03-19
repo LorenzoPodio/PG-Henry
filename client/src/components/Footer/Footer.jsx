116bf56376264787dbc967cc4f9d9042b1e20624
nabratte <61842410+nabratte@users.noreply.github.com>
2022-03-19 17:03:09 -0300

    footer stick

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import ContactUs from "../ContactUs/ContactUs";
 
 export const Footer = () => {
   return (
-    <div className="footer-container bg-gray-500 ">
+    <div className="footer-container bg-gray-500 absolute bottom-0 w-full">
       <div className="social-container">
         <p className="text">
           Realizado por Henry´s Students. Todos los derechos reservados
