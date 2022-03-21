baecf3f7449a25c3bcc10a9d6b937d6fb6c87bd4
Damian Yerien <damian_yerien@hotmail.com>
2022-03-20 21:54:50 -0300

    first

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,17 +5,96 @@ import { Link } from "react-router-dom";
 
 export const Landing = () => {
   return (
-    <div className="containerLanding h-full">
-      <img
-        className="fondoLanding h-screen w-full"
-        alt="not found"
-        src={fondo}
-      />
-      <Link to="/excursiones">
-        <button className="inline-block text-center bg-sky-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-sky-700 buttonlanding">
-          Ver Excursiones
-        </button>
-      </Link>
+    <div classNameName="container">
+    
+    <main>
+  <div className="fixed-bg bg-1">
+    
+    
+  </div>
+  <div className="fixed-bg bg-1-1">
+    <div className="text-box">
+      <h2>ExcursionApp</h2>
+      <br></br>
+      <p>
+        Disfruta de las mejores actividades
+      </p>
+      <br></br>
+      <br></br>
+      <img  src="https://image.posterlounge.es/images/l/1888768.jpg"/>
+      <p>
+      <br></br>
+      <br></br>
+      </p>
+      <p>
+      La aventura comienza 
+      </p>
+      <br></br>
+      <p>
+        donde acaban los planes
+      </p>
+      <br></br>
+      <p>
+        ...y tu aventura comienza en ExcursionApp.
+      </p>
+      <br></br>
+      <br></br>
+      <p>
+       
+      </p>
+      <br></br>
+    </div>
+  </div>
+  <div className="fixed-bg bg-2">
+    <h1>Las Mejores experiencias</h1>
+  </div>
+  <div className="fixed-bg bg-2-1">
+    <div className="text-box">
+      <h2>Descubrir</h2>
+      <p>
+        
+      </p>
+      <img src="https://royalenfieldcordoba.com.ar/wp-content/uploads/2021/01/ruta-40-moto1.jpg"/>
+      
+      <p>
+     
+      </p>
+    </div>
+  </div>
+  <div className="fixed-bg bg-3">
+    <h1>Disfrutar</h1>
+  </div>
+  <div className="fixed-bg bg-3-1">
+    <div className="text-box">
+      <h2>Title</h2>
+      <p>
+        
+      </p>
+      <img src="https://drive.google.com/file/d/1YiTlLeq09HpVItI-yLQzM6sEzHYfL_Ut/view?usp=sharing"/>
+      <p>
+      Un Lugar donde podes encontrar to.
+      </p>
+      <p>
+        He lives with his wife and it was built in the 1970s the village could only be accessed by boat.
+      </p>
+      <p>
+        He lives with his wife and thewas built in the 1970s the village could only be accessed by boat.
+      </p>
+      <p>
+        He lives with his wife and their  built in the 1970s the village could only be accessed by boat.
+      </p>
+    </div>
+  </div>
+  <div className="fixed-bg bg-4">
+    <h1>The capital is Tórshavn.</h1>
+  </div>
+  <div className="fixed-bg">
+    <h2>Thank You</h2>
+    <p>
+      Empower the Convergence of Strategic Analytics
+    </p>
+  </div>
+</main>
     </div>
   );
 };
