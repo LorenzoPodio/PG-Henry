b7c076986fa332b6a3d3d2440e973db9d3c80c06
gregoconst <gregorio.constante12@hotmail.com>
2022-03-22 16:44:19 -0300

    cambios en addReview +comentarios

--- api/src/routes/reviews/addReview.js
+++ api/src/routes/reviews/addReview.js
@@ -3,7 +3,7 @@ const addReview = Router();
 const { Reviews, User, Order, Product, Excursion } = require("../../db");
 
 addReview.post("/:id", async (req, res, next) => {
-  const { title, description, rating, email } = req.body;
+  const { description, rating, email } = req.body;
   const { id } = req.params;
   try {
     const getUserId = await User.findOne({
@@ -15,7 +15,7 @@ addReview.post("/:id", async (req, res, next) => {
 
     const orders = await Order.findAll({
       where: {
-        status: ["completed"],
+        status: ["buying"], //esto iría en completed pero esta en buying para testear
         userId: getUserId.dataValues.id,
       },
       include: [
@@ -48,13 +48,20 @@ addReview.post("/:id", async (req, res, next) => {
 
     if (buyedExcursion?.includes(excursion?.dataValues?.name)) {
       await Reviews.create({
-        // title: title,
         description: description,
         rating: rating,
         userId: getUserId.dataValues.id,
         excursionId: id,
       });
-      return res.status(201).send("Opinión agregada");
+      const response = await Reviews.findAll({ //posibilidad de mandar como respuesta directamente el array de opiniones
+        where: {
+          excursionId: id,
+        },
+        include: [
+          {model: User, attributes:["name", "lastName"]}
+      ]
+      });
+      return res.status(201).send(response); //en caso de no usar "response" cambiar el mensaje en el .send y comentar las lineas 57-64
     }
     return res
       .status(404)
