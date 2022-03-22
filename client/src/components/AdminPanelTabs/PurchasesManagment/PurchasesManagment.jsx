6947791182734879ebc966a5a36e6c1c7515f7c9
lautarocoello <lautarocoello@gmail.com>
2022-03-22 19:26:53 -0300

    matc contacto clientes con productos vendidos, panel compras sin ventas cartel informativo

--- client/src/components/AdminPanelTabs/PurchasesManagment/PurchasesManagment.jsx
+++ client/src/components/AdminPanelTabs/PurchasesManagment/PurchasesManagment.jsx
@@ -55,92 +55,139 @@ export const PurchasesManagment = () => {
   }
 
   return (
-
+    
     <div className="grid place-content-center" id="top">
-      <div className="flex flex-col w-fit m-10">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="pb-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow-lg shadow-gray-500 overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-sky-600 text-white">
-                  <tr>
-                    <th scope="col" className="px-10 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      ID
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Fecha Compra
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Detalle
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Precio Total
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Estatus
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Cliente
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                      Acciones
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {allOrders &&
-                    allOrders.map((e) => {
-                      return (
-                        <tr key={e.id}>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="flex items-center">
-                              <div className="ml-4">
-                                <div className="text-sm font-medium text-gray-900">
-                                  {e.id}
-                                </div>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-gray-900">
-                              {e.date}
-                            </div>
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                            Excursiones: {concatNames(e.products)}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                            ${sumSubTotals(e.order_details)}
-                          </td>
-                          <td className={`px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-500 ${conditionalStyle(e.status)}`}>
-                            {e.status.toUpperCase()}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                            {e.user.name} - {e.user.email}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                            <span className="hidden sm:block">
-                              <button onClick={(e) => handleCancelled(e)}
-                                disabled={e.status.toUpperCase()==='COMPLETED'? false : true}
-                                type="button"
-                                value={e.id}
-                                name={e.name}
-                                className="inline-flex px-4 py-2 shadow-lg shadow-gray-500 hover:shadow-black rounded-md text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                              >
-                                <ExclamationIcon className="-ml-1 mr-2 h-5 w-5 text-white-500" aria-hidden="true" />
-                                Cancelar
-                              </button>
-                            </span>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
+      {!allOrders.length ? (
+        <div className="flex flex-col w-fit m-10">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="pb-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow-lg shadow-gray-500 overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <h1 className="grid rounded-md shadow-lg shadow-gray-500 w-70 px-5 py-6 text-white bg-slate-700 mt-0 mb-0 font-bold text-xl">
+                  Aun no tienes ventas realizadas, realiza una primer venta !
+                </h1>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col w-fit m-10">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="pb-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow-lg shadow-gray-500 overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-sky-600 text-white">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-10 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        ID
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Fecha Compra
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Detalle
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Precio Total
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Estatus
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Cliente
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Acciones
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {allOrders &&
+                      allOrders.map((e) => {
+                        return (
+                          <tr key={e.id}>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="flex items-center">
+                                <div className="ml-4">
+                                  <div className="text-sm font-medium text-gray-900">
+                                    {e.id}
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">
+                                {e.date}
+                              </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                              Excursiones: {concatNames(e.products)}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                              ${sumSubTotals(e.order_details)}
+                            </td>
+                            <td
+                              className={`px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-500 ${conditionalStyle(
+                                e.status
+                              )}`}
+                            >
+                              {e.status.toUpperCase()}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                              {e.user.name} - {e.user.email}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                              <span className="hidden sm:block">
+                                <button
+                                  onClick={(e) => handleCancelled(e)}
+                                  disabled={
+                                    e.status.toUpperCase() === "COMPLETED"
+                                      ? false
+                                      : true
+                                  }
+                                  type="button"
+                                  value={e.id}
+                                  name={e.name}
+                                  className="inline-flex px-4 py-2 shadow-lg shadow-gray-500 hover:shadow-black rounded-md text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                                >
+                                  <ExclamationIcon
+                                    className="-ml-1 mr-2 h-5 w-5 text-white-500"
+                                    aria-hidden="true"
+                                  />
+                                  Cancelar
+                                </button>
+                              </span>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
