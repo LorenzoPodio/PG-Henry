c7b61e1ad83816fdb6c5f60659dd28b50a1d87f7
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-23 02:20:15 -0300

    Se elimino un Link de mas

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
 import { useExcursionsContext } from "../../context/ExcursionsContext";
-import { Link } from "react-router-dom";
 
 export function Profile() {
   const { submitData } = useExcursionsContext();
